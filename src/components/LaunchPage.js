9ae77632ce67d3a52b9fe5840021a34877422450
frank@gmail.com <imafidonfrank2015@gmail.com>
2024-09-25 09:02:23 +0100

    first commit

--- src/components/LaunchPage.js
+++ src/components/LaunchPage.js
@@ -12,31 +12,30 @@ const LaunchPage = ({mobile}) => {
 
 
     return(
-        <Container>
-        <Segment vertical style={{backgroundColor: '#133467', margin: mobile ? 10 : 40}}>
-                
-                            <Segment vertical style={{backgroundColor: '#3A54AF', margin: mobile ? 30: 60}}>
-                               
-                                            <Segment vertical style={{backgroundColor: '#3E72C0', margin: mobile ? 30 : 60}}>
-                                                <Grid textAlign="center" verticalAlign="middle">
-                                                    <Grid.Row>
-                                                        <Grid.Column>
-                                                            <Header style={{ margin: 10 }} textAlign="center" as="h1" content="MASTA PLANA" inverted  />
-                                                            <Loader inverted active inline size="big">
-                                                                Loading,,,
-                                                            </Loader>
-                                                        </Grid.Column>
-                                                    </Grid.Row>
-                                                </Grid>
+        
+        <Segment  vertical style={{ backgroundColor: '#133467'}}>
+                <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                    <Grid.Column style ={{maxWidth: 400}}>
+                    <Segment vertical style={{backgroundColor: '#3A54AF'}}>     
+                        <Segment vertical style={{backgroundColor: '#3E72C0', margin: mobile ? 30 : 60}}>
+                            <Grid textAlign="center" verticalAlign="middle">
+                                <Grid.Row>
+                                    <Grid.Column>
+                                        <Header style={{ margin: 10 }} textAlign="center" as="h1" content="MASTA PLANA" inverted  />
+                                        <Loader inverted active inline size="big">
+                                            Loading,,,
+                                        </Loader>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
 
-                                            </Segment>
-                            </Segment>
-                        
+                        </Segment>
+                </Segment>
+                </Grid.Column>
+                </Grid>
+                
         </Segment>
-        </Container>
 
     )
-    
-
 }
 export default LaunchPage
